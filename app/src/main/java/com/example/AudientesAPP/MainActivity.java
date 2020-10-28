d85f9d9314a6e0fa65dab05f3f44eca9e0a15fa1
デビッド <s147197@student.dtu.dk>
2020-10-28 13:34:52 +0100

    created navgiation between fragments in the navigation_graph.xml and implemented the navhost, navcontroller and bottommenu in main

--- app/src/main/java/com/example/AudientesAPP/MainActivity.java
+++ app/src/main/java/com/example/AudientesAPP/MainActivity.java
@@ -1,21 +1,67 @@
 package com.example.AudientesAPP;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
-
 import android.os.Bundle;
-import android.widget.Button;
+import android.view.MenuItem;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity implements
+        BottomNavigationView.OnNavigationItemSelectedListener {
+
+
+    private NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        NavHostFragment
+        //Navigations komponenten indeholder en default implementation af Navhost (NavHostFragment)
+        //der viser destinationer af typen fragmenter
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().
+                findFragmentById(R.id.navHost);
+
+        //Navcontrolleren er et objekt der styrer navigatioen inde i Navhosten. dvs den står for
+        //koordinationen af at skifte mellem forskellige destinationer
+        navController = navHostFragment.getNavController();
 
 
+        //laver en instans af bundmenuen og gør den synlig
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setVisibility(View.VISIBLE);
+
+        //Sætter en listener, så man kan håndterer hvad der skal ske, når der trykkes på et
+        //menu item
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
 
 
     }
+
+    //Her implementeres listeneren, hvor vi skal definerer hvad der sker når der trkker på
+    // et menu item
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+        //eks. hvis der trykkes på Library item, skal navcontrolleren navigerer til fragmentet
+        //libraryMain
+        if (menuItem.getItemId() == R.id.ic_library)
+            navController.navigate(R.id.libraryMain);
+
+        else if (menuItem.getItemId() == R.id.ic_preset)
+            navController.navigate(R.id.presetMain);
+
+        else if (menuItem.getItemId() == R.id.ic_hearingTest)
+            navController.navigate(R.id.hearingtestMain);
+
+
+        return false;
+    }
+
+}
+
+
