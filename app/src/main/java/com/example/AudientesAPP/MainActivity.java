62a8248b752e128dcb863e6d3ecc7e8e0c3e66f4
デビッド <s147197@student.dtu.dk>
2020-10-28 13:10:27 +0100

    Added nessesary dependiencies to support NavHost

--- app/src/main/java/com/example/AudientesAPP/MainActivity.java
+++ app/src/main/java/com/example/AudientesAPP/MainActivity.java
@@ -1,33 +1,21 @@
 package com.example.AudientesAPP;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
-    Button knap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        knap = findViewById(R.id.button1);
-        knap.setOnClickListener(this);
 
-        Fragment bottonMenuFrag = new BottomMenu();
-        getSupportFragmentManager().beginTransaction().add(R.id.Buttonmenu, bottonMenuFrag).commit();
+        NavHostFragment
+
 
 
 
     }
-
-    @Override
-    public void onClick(View v) {
-
-        Fragment bottonMenuFrag = new BottomMenu();
-        getSupportFragmentManager().beginTransaction().replace(R.id.Buttonmenu, bottonMenuFrag).commit();
-    }
-}
