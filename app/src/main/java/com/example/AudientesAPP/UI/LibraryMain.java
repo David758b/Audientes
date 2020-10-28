03600646f718d21e75abe2478491893338095587
デビッド <s147197@student.dtu.dk>
2020-10-28 13:58:24 +0100

    added test-templates for each fragment so that the navigation is visible

--- app/src/main/java/com/example/AudientesAPP/UI/LibraryMain.java
+++ app/src/main/java/com/example/AudientesAPP/UI/LibraryMain.java
@@ -1,6 +1,21 @@
 package com.example.AudientesAPP.UI;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 import androidx.fragment.app.Fragment;
+import com.example.AudientesAPP.R;
 
 public class LibraryMain extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View rod = inflater.inflate(R.layout.library_main, container, false);
+        TextView text = rod.findViewById(R.id.textViewTest1);
+
+        return rod;
+    }
 }
