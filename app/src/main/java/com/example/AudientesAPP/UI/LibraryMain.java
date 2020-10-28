ab0ca407ecab026165561f20709a3ad3aebc273a
デビッド <s147197@student.dtu.dk>
2020-10-28 16:10:09 +0100

    created relative layout for the list elements of the category list category_list_element.xml

--- app/src/main/java/com/example/AudientesAPP/UI/LibraryMain.java
+++ app/src/main/java/com/example/AudientesAPP/UI/LibraryMain.java
@@ -4,17 +4,26 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import android.widget.TextView;
 import androidx.fragment.app.Fragment;
 import com.example.AudientesAPP.R;
 
 public class LibraryMain extends Fragment {
 
+    private ListView categoryList;
+    int positionInList = 0;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View rod = inflater.inflate(R.layout.library_main, container, false);
-        TextView text = rod.findViewById(R.id.textViewTest1);
+
+
+
+
+
+        //TextView text = rod.findViewById(R.id.textViewTest1);
 
         return rod;
     }
