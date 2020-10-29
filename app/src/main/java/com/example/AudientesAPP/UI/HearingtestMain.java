5f2c9e99a114c9e2d1172a15d99288f0ed08f7b8
JohanKryger <s195491@student.dtu.dk>
2020-10-29 12:28:18 +0100

    Created the hearingtest image

--- app/src/main/java/com/example/AudientesAPP/UI/HearingtestMain.java
+++ app/src/main/java/com/example/AudientesAPP/UI/HearingtestMain.java
@@ -1,5 +1,8 @@
 package com.example.AudientesAPP.UI;
 
+import android.content.Intent;
+import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,14 +12,35 @@ import android.widget.TextView;
 import androidx.fragment.app.Fragment;
 import com.example.AudientesAPP.R;
 
-public class HearingtestMain extends Fragment {
+public class HearingtestMain extends Fragment implements View.OnClickListener {
 
+    private ImageView testButton;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rod = inflater.inflate(R.layout.hearingtest_main, container,
                 false);
         ImageView imageView = (ImageView) rod.findViewById(R.id.hearingTestImage);
+
+        testButton = (ImageView) rod.findViewById(R.id.hearingtestbutton);
+        testButton.setOnClickListener(this);
+
+
         return rod;
     }
+
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("market://details?id=dk.alen.audientes"));
+        try{
+            startActivity(intent);
+        }
+        catch(Exception e){
+            intent.setData(Uri.parse("https://play.google.com/store/apps/details?id=dk.alen.audientes"));
+        }
+
+    }
 }
+
