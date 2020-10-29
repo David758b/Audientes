dd0d9405338edf855086e6b77bd39113e9031713
デビッド <s147197@student.dtu.dk>
2020-10-29 11:45:04 +0100

    Applied colors and symbols to listelements

--- app/src/main/java/com/example/AudientesAPP/UI/LibraryMain.java
+++ app/src/main/java/com/example/AudientesAPP/UI/LibraryMain.java
@@ -1,5 +1,7 @@
 package com.example.AudientesAPP.UI;
 
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -26,8 +28,7 @@ public class LibraryMain extends Fragment implements AdapterView.OnItemClickList
         final View rod = inflater.inflate(R.layout.library_main, container, false);
 
         //todo dette er mega hardcoded... sæt det ind i en ressurcefil istedet og referer den
-        String[] lande = {"Presets", "Norge", "Sverige", "Japan", "Kina", "Afganistan", "Karstenstan"
-        ,"DTU-stan", "asfdjkfadsæ", "lallal", "lalalalal", "lalalal"};
+        String[] lande = {"Presets", "Sleep", "Nature", "Ocean", "Music"};
 
         ArrayAdapter arrayAdapter = new ArrayAdapter(getActivity(),
                 R.layout.category_list_element, R.id.textViewCategoryELement, lande) {
@@ -41,16 +42,35 @@ public class LibraryMain extends Fragment implements AdapterView.OnItemClickList
                 TextView textView = rod.findViewById(R.id.textViewCategoryELement);
 
 
+                //hardcoding for at skifte billeder og tekst for hvert liste element
                 switch(position){
 
                     //preset category
-                    case 0: imageView.setImageResource(R.drawable.ic_baseline_tune_24);
+                    case 0: {imageView.setImageResource(R.drawable.ic_baseline_tune_24);
+                    //sætter en nu farve uden at ændre formen på objektet
+                    view.getBackground().setColorFilter(Color.WHITE, PorterDuff.Mode.LIGHTEN);}
+
                     break;
-                    case 1: imageView.setImageResource(R.drawable.ic_baseline_tune_24);
-                    default: System.out.println("blalalal");
+                    //sleep category
+                    case 1: {imageView.setImageResource(R.drawable.ic_sleep_category);
+                        view.getBackground().setColorFilter(Color.GRAY, PorterDuff.Mode.LIGHTEN);}
+                    break;
+                    //nature category
+                    case 2: {imageView.setImageResource(R.drawable.ic_nature2_category);
+                        view.getBackground().setColorFilter(Color.GREEN, PorterDuff.Mode.LIGHTEN);}
+                    break;
+                    //waves category
+                    case 3: {imageView.setImageResource(R.drawable.ic_waves_category);
+                        view.getBackground().setColorFilter(Color.BLUE, PorterDuff.Mode.LIGHTEN);}
+                    break;
+                    //music category
+                    case 4: {imageView.setImageResource(R.drawable.ic_music_category);
+                        view.getBackground().setColorFilter(Color.YELLOW, PorterDuff.Mode.LIGHTEN);}
+                    break;
+                    default: Log.d("List error",
+                            "Error in applying image to list element");
                     break;
                 }
-
 
 
                 return view;
