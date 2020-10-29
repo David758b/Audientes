bfb9d75d8043fd80c69c58b4959987f3d5871cbd
JohanKryger <s195491@student.dtu.dk>
2020-10-29 11:38:41 +0100

    Created the hearingtest image

--- app/src/main/java/com/example/AudientesAPP/UI/HearingtestMain.java
+++ app/src/main/java/com/example/AudientesAPP/UI/HearingtestMain.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.fragment.app.Fragment;
 import com.example.AudientesAPP.R;
@@ -11,11 +12,11 @@ import com.example.AudientesAPP.R;
 public class HearingtestMain extends Fragment {
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
-        View rod = inflater.inflate(R.layout.hearingtest_main, container, false);
-        TextView text = rod.findViewById(R.id.textViewTest3);
-
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rod = inflater.inflate(R.layout.hearingtest_main, container,
+                false);
+        ImageView imageView = (ImageView) rod.findViewById(R.id.hearingTestImage);
         return rod;
     }
 }
