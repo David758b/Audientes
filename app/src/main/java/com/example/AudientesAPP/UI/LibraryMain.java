2d4abf91f8718abc2c85aae334b7a00d146032b9
Nafiu <s184174@student.dtu.dk>
2020-11-26 16:40:11 +0100

    Tabs til LibraryMain done

--- app/src/main/java/com/example/AudientesAPP/UI/LibraryMain.java
+++ app/src/main/java/com/example/AudientesAPP/UI/LibraryMain.java
@@ -1,6 +1,9 @@
 package com.example.AudientesAPP.UI;
 
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.provider.CalendarContract;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
@@ -8,15 +11,20 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.AudientesAPP.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.tabs.TabLayout;
 
-public class LibraryMain extends Fragment implements View.OnClickListener {
+public class LibraryMain extends Fragment implements View.OnClickListener{
     private MainActivity mainActivity;
     private NavController navListController;
     private TextView categories;
@@ -33,16 +41,46 @@ public class LibraryMain extends Fragment implements View.OnClickListener {
                 .findFragmentById(R.id.List_Navhost);
 
         navListController = navHostFragment.getNavController();
-        categories = rod.findViewById(R.id.categories_page);
+
+        final TabLayout tabLayout = rod.findViewById(R.id.tabs);
+        // Tabs skal måske oprettes i xml, hvor color også skal sættes
+        tabLayout.addTab(tabLayout.newTab().setText("Categories"));
+        tabLayout.addTab(tabLayout.newTab().setText("Sounds"));
+        tabLayout.setSelectedTabIndicatorColor(getResources().getColor(R.color.AudientesOrange));
+        tabLayout.setTabTextColors(getResources().getColor(R.color.LightGrey),getResources().getColor(R.color.AudientesOrange));
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                if (tab.getPosition() == 0) {
+                    navListController.navigate(R.id.libraryListCategory);
+                } else if (tab.getPosition() == 1) {
+                    navListController.navigate(R.id.libraryListSound);
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+        /*categories = rod.findViewById(R.id.categories_page);
         sounds = rod.findViewById(R.id.sounds_page);
         categories.setOnClickListener(this);
-        sounds.setOnClickListener(this);
+        sounds.setOnClickListener(this);*/
+
+
 
         mainActivity = (MainActivity) navHostFragment.getParentFragment().getActivity();
 
 
         return rod;
     }
+
 
     public MainActivity getMainActivity() {
         return mainActivity;
