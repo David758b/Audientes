b23a644210fcee919307e92e6f9c67ebf9332e9b
デビッド <s147197@student.dtu.dk>
2021-01-05 17:29:15 +0100

    læst lydfil ind i external data og dvs databehandling

--- app/src/main/java/com/example/AudientesAPP/UI/MainActivity.java
+++ app/src/main/java/com/example/AudientesAPP/UI/MainActivity.java
@@ -4,15 +4,25 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
-
+import android.content.Context;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
-
 import com.example.AudientesAPP.R;
 import com.example.AudientesAPP.model.funktionalitet.LydAfspiller;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 
 public class MainActivity extends AppCompatActivity implements
         BottomNavigationView.OnNavigationItemSelectedListener {
@@ -49,7 +59,73 @@ public class MainActivity extends AppCompatActivity implements
 
         lydAfspiller = new LydAfspiller(test, this);
 
+
+        //---------------------------------------------------------------------
+        // todo ---- virker. nu skal vi bare hente filen igen og afspille den
+        String dir = this.getExternalFilesDir(Environment.DIRECTORY_MUSIC).toString();
+        System.out.println(dir);
+        //storage/emulated/0/Android/data/com.example.audientesapp/files/Music
+
+
+        File data = getAppSpecificAlbumStorageDir(this, "Audientes");
+
+
+        // Alt skal ligge der!
+        System.out.println(data.getAbsolutePath());
+        // /storage/emulated/0/Android/data/com.example.audientesapp/files/Music/Audientes
+
+        File chihua = new File(data, "chihuahua");
+        try{
+
+
+
+            System.out.println(chihua.getAbsolutePath());
+            ///storage/emulated/0/Android/data/com.example.audientesapp/files/Music/Audientes/chihuahua
+
+            System.out.println(chihua.length());
+            //378471 BYTES
+
+
+
+        InputStream in = getResources().openRawResource(R.raw.chihuahua);
+        FileOutputStream out = new FileOutputStream(chihua.getAbsolutePath());
+        byte[] buff = new byte[1024];
+        int read = 0;
+
+        try {
+            while ((read = in.read(buff)) > 0) {
+                out.write(buff, 0, read);
+            }
+        } finally {
+            in.close();
+            out.close();
+            System.out.println("DET VIRKER!!!!");
+        }
+
+        }catch (Exception e){
+            System.out.println("FAAAAAAAAAAAAAAIIILLLLL");
+            e.printStackTrace();
+        }
+
+
+        //test ----det virker sgu !!!
+        try {
+            MediaPlayer mPlayer = new MediaPlayer();
+            Uri myUri = Uri.parse(chihua.getAbsolutePath());
+            mPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+            mPlayer.setDataSource(getApplicationContext(), myUri);
+            mPlayer.prepare();
+            mPlayer.start();
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
+
+
+
+
+
 
     //Her implementeres listeneren, hvor vi skal definerer hvad der sker når der trkker på
     // et menu item
@@ -75,6 +151,101 @@ public class MainActivity extends AppCompatActivity implements
 
     public LydAfspiller getLydAfspiller() {
         return lydAfspiller;
+    }
+
+    File getAppSpecificAlbumStorageDir(Context context, String albumName) {
+        // Get the pictures directory that's inside the app-specific directory on
+        // external storage.
+        File file = new File(context.getExternalFilesDir(
+                Environment.DIRECTORY_MUSIC), albumName);
+        if (file == null || !file.mkdirs()) {
+            System.out.println("DEN FINDES ALLEREDE");
+        }
+        return file;
+    }
+
+    private void copyFile(String inputPath, String inputFile, String outputPath) {
+
+        InputStream in = null;
+        OutputStream out = null;
+        try {
+
+            //create output directory if it doesn't exist
+            File dir = new File (outputPath);
+            if (!dir.exists())
+            {
+                dir.mkdirs();
+            }
+
+
+            in = new FileInputStream(inputPath + inputFile);
+            out = new FileOutputStream(outputPath + inputFile);
+
+            byte[] buffer = new byte[1024];
+            int read;
+            while ((read = in.read(buffer)) != -1) {
+                out.write(buffer, 0, read);
+            }
+            in.close();
+            in = null;
+
+            // write the output file (You have now copied the file)
+            out.flush();
+            out.close();
+            out = null;
+
+        }  catch (FileNotFoundException fnfe1) {
+            Log.e("tag", fnfe1.getMessage());
+        }
+        catch (Exception e) {
+            Log.e("tag", e.getMessage());
+        }
+
+    }
+
+    private void moveFile(String inputPath, String inputFile, String outputPath) {
+
+        InputStream in = null;
+        OutputStream out = null;
+        try {
+
+            //create output directory if it doesn't exist
+            File dir = new File (outputPath);
+            if (!dir.exists())
+            {
+                dir.mkdirs();
+            }
+
+
+            in = new FileInputStream(inputPath + inputFile);
+            out = new FileOutputStream(outputPath + inputFile);
+
+            byte[] buffer = new byte[1024];
+            int read;
+            while ((read = in.read(buffer)) != -1) {
+                out.write(buffer, 0, read);
+            }
+            in.close();
+            in = null;
+
+            // write the output file
+            out.flush();
+            out.close();
+            out = null;
+
+            // delete the original file
+            new File(inputPath + inputFile).delete();
+
+
+        }
+
+        catch (FileNotFoundException fnfe1) {
+            Log.e("tag", fnfe1.getMessage());
+        }
+        catch (Exception e) {
+            Log.e("tag", e.getMessage());
+        }
+
     }
 }
 
