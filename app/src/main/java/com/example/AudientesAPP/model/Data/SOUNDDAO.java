5f03e8599112971a05abf6b3c71ee4597d7d3056
Nafiu <s184174@student.dtu.dk>
2021-01-08 10:05:29 +0100

    Branch fixing

--- app/src/main/java/com/example/AudientesAPP/model/Data/SOUNDDAO.java
+++ app/src/main/java/com/example/AudientesAPP/model/Data/SOUNDDAO.java
@@ -5,50 +5,6 @@ import com.example.AudientesAPP.DTO.PresetItemDTO;
 
 import java.util.List;
 
-public class SOUNDDAO implements IDAO{
+public class SOUNDDAO{
 
-    @Override
-    public void add(Object object) {
-
-    }
-
-    @Override
-    public void delete(Object object) {
-
-    }
-
-    @Override
-    public List<Object> getList() {
-        return null;
-    }
-
-    @Override
-    public List<Object> getCategorySound(Object SoundDTO) {
-        return null;
-    }
-
-    @Override
-    public List<Object> getSoundCategory(Object CategoryDTO) {
-        return null;
-    }
-
-    @Override
-    public List<Object> getPresetCategory(Object CategoryDTO) {
-        return null;
-    }
-
-    @Override
-    public List<Object> getCategoryPreset(Object PresetDTO) {
-        return null;
-    }
-
-    @Override
-    public List<Object> getPresetElementer(String presetName) {
-        return null;
-    }
-
-    @Override
-    public Object getSound(String soundName) {
-        return null;
-    }
 }
